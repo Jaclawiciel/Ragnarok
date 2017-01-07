84d9094d11288968b77a1c608f04a58f4b9289a5
Jacek <jacgalka@icloud.com>
2017-01-07 02:09:02 +0100

    Dodałem klasę Path z konstruktorem

---
+++ Ragnarok/Ragnarok/Path.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ragnarok {
+	class Path {
+		private readonly MapLocation[] path;
+		public int Length => path.Length; //Wyrażenie Lambda
+
+		public Path(MapLocation[] path) {
+			this.path = path;
+		}
+	}
+}
