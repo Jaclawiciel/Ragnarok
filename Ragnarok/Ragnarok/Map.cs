2235e30c7f2b2841bcafc83e9a03466f15a7be72
Jacek <jacgalka@icloud.com>
2017-01-07 01:08:23 +0100

    Dodałem metodę sprawdzającą czy obiekt znajduje się w obrębie mapy

--- Ragnarok/Ragnarok/Map.cs
+++ Ragnarok/Ragnarok/Map.cs
@@ -17,5 +17,11 @@ namespace Ragnarok
 			Width = width;
 			Height = height;
 		}
+
+		//Metoda sprawdzająca czy element siatki (obiekt typu MapLocation) znajduje się w obrębie mapy. 
+		public bool OnMap(MapLocation location) {
+			return location.X >= 0 && location.X < Width &&
+				   location.Y >= 0 && location.Y < Height;
+		}
 	}
 }
