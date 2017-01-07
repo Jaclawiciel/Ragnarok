f7e6bc916d12ff360e112236a56a52f26ce95fd3
Jacek <jacgalka@icloud.com>
2017-01-07 14:32:23 +0100

    Dodałem klasę RagnarokException dla obsługi wyjątków

---
+++ Ragnarok/Ragnarok/RagnarokException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ragnarok {
+	class RagnarokException : Exception {
+		public RagnarokException() {
+
+		}
+
+		public RagnarokException(string message) : base(message) {
+
+		}
+	}
+
+	class OutOfBoundsException : RagnarokException {
+		public OutOfBoundsException() {
+
+		}
+
+		public OutOfBoundsException(string message) : base(message) {
+
+		}
+	}
+}
