27d1657e1bc496a014c6a8e0d891ca0e1e409e5b
Michał Dominik <priv@michaldominik.pl>
2017-01-23 16:33:11 +0100

    Metoda game, potrafi usuwać i dodawać wieże.

--- Ragnarok/Ragnarok/Level.cs
+++ Ragnarok/Ragnarok/Level.cs
@@ -6,12 +6,6 @@ using System.Threading.Tasks;
 
 namespace Ragnarok {
     class Level {
-        public Map map = new Map(15, 10);
-        public List<Tower> towers = new List<Tower>(); //nie można zrobić get; set;
-        public List<IInvader> invaders = new List<IInvader>();
-        public Path path = new Path(new[] { new MapLocation(0, 5, map), new MapLocation(1, 5, map), new MapLocation(2, 5, map), new MapLocation(3, 5, map),
-             new MapLocation(4, 5, map), new MapLocation(5, 5, map), new MapLocation(6, 5, map), new MapLocation(7, 5, map),
-             new MapLocation(8, 5, map), new MapLocation(9, 5, map), new MapLocation(10, 5, map), new MapLocation(11, 5, map),
-             new MapLocation(12, 5, map), new MapLocation(13, 5, map), new MapLocation(14, 5, map), new MapLocation(15, 5, map)});
+
     }
 }
