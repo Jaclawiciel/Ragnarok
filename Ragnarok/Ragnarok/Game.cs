3e3017124e72ca731746a35d1fe80348b16768c1
Michał Dominik <priv@michaldominik.pl>
2017-01-23 21:54:43 +0100

    Dalsze prace nad PlayTurn

--- Ragnarok/Ragnarok/Game.cs
+++ Ragnarok/Ragnarok/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Ragnarok {
 	class Game {
@@ -11,6 +12,7 @@ namespace Ragnarok {
         public List<Tower> towers { get; private set; }
         public List<IInvader> invaders { get; private set; }
         public int waveCount { get; private set; } = 3;
+        public int zycia { get; private set; } = 5;
 
         public Game() {
             path =new Path(new[] { new MapLocation(0, 5, map), new MapLocation(1, 5, map), new MapLocation(2, 5, map), new MapLocation(3, 5, map),
@@ -24,6 +26,10 @@ namespace Ragnarok {
 
         public void PlayTurn() {
 
+            MoveEveryone();
+            AreYouAlive();
+            WaveSpawn();
+            CorpseRemoval();
         }
         //przekazujesz tu tower w sposób AddTower(new TypWieży(new MapLocation(X, Y, map)));
         public void AddTower(Tower tower) {
@@ -48,8 +54,35 @@ namespace Ragnarok {
                     break;
             }
         }
+
+        public void MoveEveryone() {
+            foreach(IInvader invader in invaders) {
+                if (invader.IsActive) {
+                    invader.Move();
+                    if (invader.PathEnded) zycia -= 1;
+                }
+            }
+        }
+        public void AreYouAlive() {
+            if (zycia <= 0) Dead();
+        }
+        public void Dead() {
+            MessageBox.Show("YOU LOST !");
+            towers = new List<Tower>();
+            invaders = new List<IInvader>();
+            waveCount = 3;
+            zycia = 5;
+        }
         public void WaveSpawn() {
-            if (invaders.Count() <= waveCount) AddInvader();
+            if (invaders.Count() < waveCount) AddInvader();
+        }
+        public void CorpseRemoval() {
+            bool temp = false;
+            foreach(IInvader invader in invaders) {
+                if (invader.IsActive) temp = true;
+            }
+            if (!temp) invaders = new List<IInvader>();
+            waveCount += 1;
         }
 	}
 }
